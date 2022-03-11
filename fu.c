752adeafcc4ed2b05fc4072a54d3bc8b93e10371
付青云 <1366773912@qq.com>
2022-03-11 10:17:26 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5 };
+//	arr[0] = arr[1];
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int i = 0;
+//    int j = 0;
+//    char a;
+//    char arr[3][3];
+//    scanf("%c", &a);
+//    for (i = 0; i <3; i++)
+//    {
+//        for (j=0;j<3;j++)
+//        {
+//            arr[i][j] = a;
+//        }
+//    }
+//    for (i = 0; i < 3; i++)
+//    {
+//        for (j = 0; j < 3; j++)
+//        {
+//            printf("%c", arr[i][j]);
+//        }  
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
